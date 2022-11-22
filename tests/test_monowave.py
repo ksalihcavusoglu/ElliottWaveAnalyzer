6826aa2a376f520ad42fe08067fbcd151918bace
ksalihcavusoglu <kemal.salihcavusoglu@windowslive.com>
2022-11-22 19:20:04 +0300

    Pytest and change log introduced.

--- tests/test_monowave.py
+++ tests/test_monowave.py
@@ -1,8 +1,10 @@
 from models.MonoWave import MonoWaveUp
+from models.MonoWave import MonoWaveDown
+
 import numpy as np
 
 
-def test_monowave_instance_is_created():
+def test_monowaveup_instance_is_created():
     lows = np.random.rand(100)
     highs = np.random.rand(100)
     dates = np.random.rand(100)
@@ -10,3 +12,12 @@ def test_monowave_instance_is_created():
     monowave_up = MonoWaveUp(lows, highs, dates, 0)
 
     assert isinstance(monowave_up, MonoWaveUp)
+
+def test_monowavedown_instance_is_created():
+    lows = np.random.rand(100)
+    highs = np.random.rand(100)
+    dates = np.random.rand(100)
+
+    monowave_down = MonoWaveDown(lows, highs, dates, 0)
+
+    assert isinstance(monowave_down, MonoWaveDown)
